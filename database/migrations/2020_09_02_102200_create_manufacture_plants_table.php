e1053a19e39736169b93b9ac49e3d65dedc3a118
anditsung <anditsung@gmail.com>
2021-05-03 14:25:42 +0700

    update database

--- database/migrations/2020_09_02_102200_create_manufacture_plants_table.php
+++ database/migrations/2020_09_02_102200_create_manufacture_plants_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateManufacturePlansTable extends Migration
+class CreateManufacturePlantsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateManufacturePlansTable extends Migration
      */
     public function up()
     {
-        Schema::create('manufacture_plans', function (Blueprint $table) {
+        Schema::create('manufacture_plants', function (Blueprint $table) {
             $table->id();
             $table->string('code')->unique();
             $table->string('name');
@@ -30,6 +30,6 @@ class CreateManufacturePlansTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('manufacture_plans');
+        Schema::dropIfExists('manufacture_plants');
     }
 }
